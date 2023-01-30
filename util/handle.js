a209b4e343989f0f1626daaa60c6453af235d9d3
MoonLGH <MoonLMC1212@gmail.com>
2023-01-30 14:04:15 +0000

    Update cmd to output generated at 617d65a

--- util/handle.js
+++ util/handle.js
@@ -21,3 +21,24 @@ export async function LoadCommands() {
     console.log("[Handler]" + commands.size + " Loaded");
     return commands;
 }
+export async function LoadCommandsNoCollection() {
+    const cmds = [];
+    const dirs = readdirSync("./dist/commands/", {
+        withFileTypes: true,
+    }).filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
+    for (const folder of dirs) {
+        for (const file of readdirSync("./dist/commands/" + folder).filter((file) => file.endsWith(".ts") || file.endsWith(".js"))) {
+            const command = await import(`../commands/${folder}/${file}`);
+            cmds.push({
+                name: command.name.toLowerCase(),
+                alias: command.alias?.map((c) => c.toLowerCase()) || [],
+                category: folder,
+                filepath: file,
+                description: command.description || "",
+                run: command.run,
+            });
+        }
+    }
+    console.log("[Handler]" + cmds.length + " Loaded");
+    return cmds;
+}
