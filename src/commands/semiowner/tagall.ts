f992ad9bd0cc1cb2cdae579878ebc9cee932a487
Moon Light「つかり」 <MoonLMC1212@gmail.com>
2023-03-12 17:41:03 +0000

    im so stupid

--- src/commands/semiowner/tagall.ts
+++ src/commands/semiowner/tagall.ts
@@ -1,18 +1,25 @@
-import {Contact, ContactId, Message} from "@open-wa/wa-automate";
-import {Client} from "../../util/extend/Client";
+import { Contact, ContactId, Message } from "@open-wa/wa-automate";
+import { Client } from "../../util/extend/Client";
 
-export function run(client:Client, message:Message) {
-  if (!message.fromMe || (client.semiOwner && client.semiOwner.includes(message.sender.id))) return client.clientInstances!.sendText(message.chatId, "You must be semi owner to use this command");
-
-  client.clientInstances?.sendReplyWithMentions(message.chatId, `${getName(message.sender)} Has Called You`, message.id, true, (message.chat.groupMetadata.participants.map((p) => p.id) as unknown as ContactId[]));
+export function run(client: Client, message: Message) {
+    // check if the message is from a semi owner
+    if (client.semiOwner?.includes(message.sender.id) || message.fromMe) {
+        if (!message.isGroupMsg) {
+            return client.clientInstances!.sendText(message.chatId, "This command only works on group chats");
+        }
+        client.clientInstances?.sendReplyWithMentions(message.chatId, `${getName(message.sender)} Has Called You`, message.id, true, (message.chat.groupMetadata.participants.map((p) => p.id) as unknown as ContactId[]));
+    } else {
+        return client.clientInstances!.sendText(message.chatId, "You Need to be semi owner or higher");
+    }
 }
+
 
 export const name = "tagall";
 
 function getName(contact: Contact) {
-  const { pushname, formattedName, verifiedName,name } = contact;
-  if(contact.isMe){
-    return "Yang Mulia"
-  }
-  return name || pushname || verifiedName || formattedName;
+    const { pushname, formattedName, verifiedName, name } = contact;
+    if (contact.isMe) {
+        return "Yang Mulia"
+    }
+    return name || pushname || verifiedName || formattedName;
 }
