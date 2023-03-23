aff754960e468415d0da30720a2b39f77bb54333
Moon Light「つかり」 <MoonLMC1212@gmail.com>
2023-03-23 03:37:48 +0000

    yes

---
+++ src/commands/images/genimages.ts
@@ -0,0 +1,29 @@
+import {Message} from "@open-wa/wa-automate";
+import {Client} from "../../util/extend/Client";
+import {Configuration, OpenAIApi} from "openai";
+import "dotenv/config";
+
+export async function run(client:Client, message:Message) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const {args} = client.parseMessage(message);
+
+  if (args.length > 0) {
+    const response = await openai.createImage({
+      prompt: args.join(" "),
+      n: 1,
+      size: "1024x1024",
+    });
+    const image_url = response.data.data[0].url;
+    client.clientInstances?.sendFileFromUrl(message.chatId, image_url!, `${args.join(" ")}.jpg`, "here you go");
+  } else {
+    client.clientInstances!.sendText(message.chatId, "enter question");
+  }
+}
+
+export const name = "genimage";
+export const description = "Ask bot powered by dall-e to generate image";
+export const alias = ["imageai"];
