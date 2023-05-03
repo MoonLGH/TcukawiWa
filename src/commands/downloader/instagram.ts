4fc90a54fd66a447d05cba0f877605a2920b5615
Moon Light「つかり」 <MoonLMC1212@gmail.com>
2023-05-03 13:23:33 +0000

    igdl fix

--- src/commands/downloader/instagram.ts
+++ src/commands/downloader/instagram.ts
@@ -2,16 +2,22 @@ import {Message} from "@open-wa/wa-automate";
 import {Client} from "../../util/extend/Client";
 import {instagram} from "../../util/instagram.js";
 export async function run(client:Client, message:Message) {
+  client.clientInstances!.sendText(message.chatId, "Please Wait");
   const {args} = client.parseMessage(message);
-  const resInsta = await instagram(args[0]);
-  if (typeof resInsta === "string") {
-    return client.clientInstances!.sendText(message.chatId, "Error has been found");
-  }
+  for (let i = 0; i < args.length; i++) {
+    const element = args[i];
+    if (element.toLowerCase().includes("instagram")) {
+      const resInsta = await instagram(element);
+      if (typeof resInsta === "string") {
+        return client.clientInstances!.sendText(message.chatId, "Error has been found");
+      }
 
-  for (let i = 0; i < resInsta.length; i++) {
-    const element:string = resInsta[i];
-    // element is url, fetch element to buffer using axios
-    client.clientInstances?.sendFileFromUrl(message.chatId, element, "instagram", "instagram");
+      for (let i = 0; i < resInsta.media.length; i++) {
+        const element:string = resInsta.media[i];
+        // element is url, fetch element to buffer using axios
+        client.clientInstances?.sendFileFromUrl(message.chatId, element, "instagram", "instagram");
+      }
+    }
   }
 }
 
