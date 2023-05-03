236992400b04750934f8a32c5f4367a06b99d41b
MoonLGH <MoonLMC1212@gmail.com>
2023-05-03 13:31:07 +0000

    Update cmd to output generated at 4fc90a5

--- commands/downloader/instagram.js
+++ commands/downloader/instagram.js
@@ -1,14 +1,20 @@
 import { instagram } from "../../util/instagram.js";
 export async function run(client, message) {
+    client.clientInstances.sendText(message.chatId, "Please Wait");
     const { args } = client.parseMessage(message);
-    const resInsta = await instagram(args[0]);
-    if (typeof resInsta === "string") {
-        return client.clientInstances.sendText(message.chatId, "Error has been found");
-    }
-    for (let i = 0; i < resInsta.length; i++) {
-        const element = resInsta[i];
-        // element is url, fetch element to buffer using axios
-        client.clientInstances?.sendFileFromUrl(message.chatId, element, "instagram", "instagram");
+    for (let i = 0; i < args.length; i++) {
+        const element = args[i];
+        if (element.toLowerCase().includes("instagram")) {
+            const resInsta = await instagram(element);
+            if (typeof resInsta === "string") {
+                return client.clientInstances.sendText(message.chatId, "Error has been found");
+            }
+            for (let i = 0; i < resInsta.media.length; i++) {
+                const element = resInsta.media[i];
+                // element is url, fetch element to buffer using axios
+                client.clientInstances?.sendFileFromUrl(message.chatId, element, "instagram", "instagram");
+            }
+        }
     }
 }
 export const name = "instagram";
