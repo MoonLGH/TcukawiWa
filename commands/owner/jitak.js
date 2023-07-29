b0a32459d2a32fcf68557bb3ccdd35ed3bfaf9b3
MoonLGH <MoonLMC1212@gmail.com>
2023-07-29 12:49:57 +0000

    Update cmd to output generated at f6d3fee

--- commands/owner/jitak.js
+++ commands/owner/jitak.js
@@ -1,14 +1,26 @@
 // create jitakcount variable with string: number type
 let jitakCount = {};
 export async function run(client, message) {
+    if (!message.fromMe)
+        return client.clientInstances?.sendText(message.chatId, "you are not me");
+    if (message.body.includes("-leaderboard")) {
+        const sorted = Object.entries(jitakCount).sort((a, b) => b[1] - a[1]);
+        const text = sorted.map((x) => `${x[0]}: ${x[1]}`).join("\n");
+        client.clientInstances?.sendText(message.chatId, `Jitak Leaderboard:\n\n${text}`);
+        return;
+    }
+    if (message.body.includes("-resetall")) {
+        jitakCount = {};
+        return client.clientInstances?.sendText(message.chatId, "Jitak count resetted");
+    }
     if (!message.body.includes("-person:")) {
         if (!jitakCount["aya"]) {
             jitakCount["aya"] = 0;
         }
         jitakCount["aya"]++;
-        client.clientInstances?.sendText(message.chatId, `Aya Jitak count: ${jitakCount["aya"]}`);
+        client.clientInstances?.sendText(message.chatId, `aya Jitak count: ${jitakCount["aya"]}`);
     }
-    if (message.body.includes("-person:")) {
+    else if (message.body.includes("-person:")) {
         const person = message.body.split("-person:")[1].split(" ")[0];
         if (!jitakCount[person]) {
             jitakCount[person] = 0;
@@ -23,14 +35,6 @@ export async function run(client, message) {
             jitakCount[person] = 0;
         }
         client.clientInstances?.sendText(message.chatId, `${person} Jitak count: ${jitakCount[person]}`);
-    }
-    if (message.body.includes("-resetAll")) {
-        jitakCount = {};
-    }
-    if (message.body.includes("-leaderboard")) {
-        const sorted = Object.entries(jitakCount).sort((a, b) => b[1] - a[1]);
-        const text = sorted.map((x) => `${x[0]}: ${x[1]}`).join("\n");
-        client.clientInstances?.sendText(message.chatId, `Jitak Leaderboard:\n\n${text}`);
     }
 }
 export const name = "jitak";
